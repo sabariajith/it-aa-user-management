648e02a3dd8d82748ab5008ebd6cd8b76fa35869
sabariajith <sabariajith@gmail.com>
2023-10-29 12:41:11 +0530

    Issue Fixes

--- src/main/java/com/example/UserManagementApplication.java
+++ src/main/java/com/example/UserManagementApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserManegementApplication {
+public class UserManagementApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserManegementApplication.class, args);
+		SpringApplication.run(UserManagementApplication.class, args);
 	}
 
 }
